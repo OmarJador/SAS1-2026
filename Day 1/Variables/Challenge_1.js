3cdb8dfbb55e0fbcd97bf9ccc84d98eb3b34ba7f
Omar <omarjador1@outlook.com>
2026-09-01 11:42:34 +0100

    Added Challenge_1.js fully functional with user inputs

---
+++ Day 1/Variables/Challenge_1.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+let Pseudo = prompt("Pseudo:");
+let Niveau = prompt("Niveau:");
+let Age = prompt("Age:");
+let Score = prompt("Score:");
+let Pays = prompt("Pays:");
+console.log("Pseudo:",Pseudo);
+console.log("Niveau:",Niveau);
+console.log("Age:",Age);
+console.log("Score:",Score);
+console.log("Pays:",Pays);
