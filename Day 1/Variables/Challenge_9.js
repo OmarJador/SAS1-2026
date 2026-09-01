2fc596354c9498898ec29a637873b12e5a7ccc8a
Omar <omarjador1@outlook.com>
2026-09-01 17:03:45 +0100

    Started Challenge_9.js.

---
+++ Day 1/Variables/Challenge_9.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+let x1 = parseInt(prompt("Valeur x du point A :"));
+let y1 = parseInt(prompt("Valeur y du point A :"));
+let z1 = parseInt(prompt("Valeur z du point A :"));
+console.log("La première position est : A(",x1,",",y1,",",z1,")")
