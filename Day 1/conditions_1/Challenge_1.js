e88c823ace550da8b4dfa115b8c2fe9b55630504
Omar <omarjador1@outlook.com>
2026-09-02 00:52:47 +0100

    Added Challenge_1.js Vérifier l'âge.

---
+++ Day 1/conditions_1/Challenge_1.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+let age = parseInt(prompt("Age :"));
+
+if (age>=18){
+    console.log("Accès autorisé")
+}else{
+    console.log("Accès non autorisé")
+};
