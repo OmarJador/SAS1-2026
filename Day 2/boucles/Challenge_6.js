076b8ae558f61db84e508636ba25080adc41269f
Omar <omarjador1@outlook.com>
2026-09-02 20:47:09 +0100

    Added Challenge_6.js Compte à rebours.

--- Day 2/boucles/Challenge_6.js
+++ Day 2/boucles/Challenge_6.js
@@ -1,6 +1,11 @@
 const prompt = require("prompt-sync")();
 
-let nmbr = parseInt(prompt("Donner un nombre :"));
-let sub = parseInt("");
+let nmbr = prompt("Tapez un nombre :");
 
+console.log("Départ :",nmbr);
 
+for(let i=0; nmbr>=0; nmbr--){
+    console.log(nmbr);
+}
+
+console.log("Décollage !");
