4c14e088a2377de12543bf10546f4e39a0ccb07c
Omar <omarjador1@outlook.com>
2026-09-02 14:49:59 +0100

    Modified Challenge_5.js, Added line 11 bonus.

--- Day 2/boucles/Challenge_5.js
+++ Day 2/boucles/Challenge_5.js
@@ -4,7 +4,8 @@ let N = parseInt(prompt("Donner un nombre N :"));
 let X = parseInt(prompt("Donner un nombre X :"));
 let mult =parseInt("0");
 
-for(i=1; mult<N; i++){
+for(i=0; mult<N; i++){
     mult = i * X;
     console.log(mult);
 };
+console.log("Nombre total de multiples trouvés :",i);
