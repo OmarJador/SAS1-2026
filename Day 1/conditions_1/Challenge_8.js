dd0bf0f40522ce939d5a9220d36562dd2ee59c60
Omar <omarjador1@outlook.com>
2026-09-02 23:57:22 +0100

    Started Challenge_8.js, work in progress.

---
+++ Day 1/conditions_1/Challenge_8.js
@@ -0,0 +1,30 @@
+const prompt = require("prompt-sync")();
+
+let nmb1 = parseFloat(prompt("Tapez le premier nombre :"))
+let oper = prompt("Votre operateur :")
+let nmb2 = parseFloat(prompt("Tapez le deuxieme nombre :"))
+let rslt = parseFloat("").toFixed(2)
+
+switch(oper){
+
+    case +:
+
+    break;
+    case -:
+
+    break;
+    case *:
+
+    break;
+    case /:
+
+    break;
+    default:
+
+    break;
+
+
+
+
+
+}
