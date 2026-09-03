e1465fa53d8332d6d654b88ae871e7d7552df8a9
Omar <omarjador1@outlook.com>
2026-09-03 09:00:53 +0100

    Finished Challenge_8.js Calculatrice simple.

--- Day 1/conditions_1/Challenge_8.js
+++ Day 1/conditions_1/Challenge_8.js
@@ -1,30 +1,35 @@
 const prompt = require("prompt-sync")();
 
-let nmb1 = parseFloat(prompt("Tapez le premier nombre :"))
-let oper = prompt("Votre operateur :")
-let nmb2 = parseFloat(prompt("Tapez le deuxieme nombre :"))
-let rslt = parseFloat("").toFixed(2)
+let nmb1 = parseFloat(prompt("Tapez le premier nombre :"));
+let oper = prompt("Votre operateur :");
+let nmb2 = parseFloat(prompt("Tapez le deuxieme nombre :"));
+let rslt = parseFloat("").toFixed(2);
 
 switch(oper){
 
-    case +:
-
+    case "+":
+        rslt = nmb1 + nmb2
+        console.log(nmb1,"+",nmb2);
+        console.log("Résultat :",rslt);
     break;
-    case -:
-
+    case "-":
+        rslt = nmb1 - nmb2
+        console.log(nmb1,"-",nmb2);
+        console.log("Résultat :",rslt);
     break;
-    case *:
-
+    case "*":
+        rslt = nmb1 * nmb2
+        console.log(nmb1,"*",nmb2);
+        console.log("Résultat :",rslt);
     break;
-    case /:
-
+    case "/":
+        rslt = nmb1 / nmb2
+        console.log(nmb1,"/",nmb2);
+        console.log("Résultat :",rslt);
     break;
     default:
-
+        console.log(nmb1,oper,nmb2);
+        console.log("Opérateur invalide.");
     break;
-
-
-
-
 
 }
