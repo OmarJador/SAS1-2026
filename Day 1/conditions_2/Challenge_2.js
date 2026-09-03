62b7b02b1a0eb40d4f3a43e1d49568f28b54ab06
Omar <omarjador1@outlook.com>
2026-09-03 13:42:21 +0100

    Added Challenge_2.js Feu tricolore.

---
+++ Day 1/conditions_2/Challenge_2.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+
+let color = prompt("Tapez une couleur du feu :").toLowerCase().trimEnd();
+
+switch(color){
+
+    case "rouge": console.log("rouge  → Arrêtez-vous");
+        break;
+    case "orange": console.log("orange → Ralentissez");
+        break;
+    case "vert": console.log("vert   → Vous pouvez passer");
+        break;
+    default:
+        console.log("Couleur invalide.")
+
+}
