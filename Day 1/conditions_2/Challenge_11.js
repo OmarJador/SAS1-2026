7b77afe7f15db6fc51b79506607ad3b7da7b0429
Omar <omarjador1@outlook.com>
2026-09-04 18:16:48 +0100

    Started Challenge_11.js Analyse d'une date.

---
+++ Day 1/conditions_2/Challenge_11.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+
+let day = parseInt(prompt("Tapez un jour (de 1 a 31) :"));
+let month = parseInt(prompt("Tapez un mois (de 1 a 12) :"));
+let year = parseInt(prompt("Tapez une annee (de 1 a 2026) :"));
+
