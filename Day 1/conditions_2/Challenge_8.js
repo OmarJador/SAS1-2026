8128c7d6601f4698b07dc644483219fd71369440
Omar <omarjador1@outlook.com>
2026-09-04 17:45:07 +0100

    Finished Challenge_8.js Niveau de difficulté d'un jeu.

--- Day 1/conditions_2/Challenge_8.js
+++ Day 1/conditions_2/Challenge_8.js
@@ -9,9 +9,29 @@ let choix = parseInt(prompt("Tapez votre choix de difficulté (de 1 à 4) :"));
 
 switch(choix){
     case 1:
-        
+        console.log("choix :",choix);
         console.log("Niveau : Facile");
         console.log("Enemis: 15");
         console.log("Points par enemi : 5");
     break;
+    case 2:
+        console.log("choix :",choix);
+        console.log("Niveau : Normal");
+        console.log("Enemis: 30");
+        console.log("Points par enemi : 15");
+    break;
+    case 3:
+        console.log("choix :",choix);
+        console.log("Niveau : Difficile");
+        console.log("Enemis: 50");
+        console.log("Points par enemi : 30");
+    break;
+    case 4:
+        console.log("choix :",choix);
+        console.log("Niveau : Extreme");
+        console.log("Enemis: 100");
+        console.log("Points par enemi : 100");
+    break;
+    default:
+        console.log("Choix invalide.")
 }
