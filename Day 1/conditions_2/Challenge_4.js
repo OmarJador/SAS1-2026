1cfeec0b4b04dc5b9620534ab345302f22a5d3cd
Omar <omarjador1@outlook.com>
2026-09-04 15:58:49 +0100

    started Challenge_4.js Validation d'un triangle.

---
+++ Day 1/conditions_2/Challenge_4.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+
+let A = parseInt(prompt("Donnez la longueur du coté A :"));
+let B = parseInt(prompt("Donnez la longueur du coté B :"));
+let C = parseInt(prompt("Donnez la longueur du coté C :"));
+
+// if(){
+    
+//     console.log("Triangle Scalène.");
+
+// }else if(cotéA == cotéB == cotéC){
+    
+//     console.log("Triangle Équilatéral.");
+
+// }
+
