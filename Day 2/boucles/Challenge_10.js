1d40dc615acc545c25310baeb9f15b5bf774b472
Omar <omarjador1@outlook.com>
2026-09-04 21:35:51 +0100

    Modified Challenge_10.js

--- Day 2/boucles/Challenge_10.js
+++ Day 2/boucles/Challenge_10.js
@@ -5,7 +5,7 @@ let i=0;
 
 while(1){
 
-    let nmbr = parseInt(prompt("Donnez un nombre a ajouter :"));
+    let nmbr = parseInt(prompt("Tapez un nombre a ajouter (entrez 0 pour calculer):"));
     if(nmbr == 0) break;
     somme = somme +nmbr;
     i++
