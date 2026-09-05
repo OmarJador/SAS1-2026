b822150042ecf2d95a50009d3ec7ea9953ed142b
Omar <omarjador1@outlook.com>
2026-09-05 13:55:47 +0100

    Added exercise d'erreur

---
+++ exercise.js
@@ -0,0 +1,96 @@
+const prompt = require ("prompt-sync")();
+
+// let unit = parseInt(prompt("Prix unitaire :"));
+// let qua = parseInt(prompt("Quantité :"));
+// let argent = parseInt(prompt("Argent :"));
+// let total = unit * qua;
+// let rest = 0;
+// final =0;
+// red = 0;
+
+// if(argent>= total){
+//     if(total>=500){
+
+//         red = total *.5
+//         final = total - red;
+//         rest = argent - final;
+
+
+//     }else if(total >= 200){
+
+//         red = total *.2
+//         final = total - red;
+//         rest = argent - final;
+
+
+//     }else if(total >=100){
+//         red = total *.1
+//         final = total - red;
+//         rest = argent - final;
+
+//     }else{
+
+//         final = total - red;
+//         rest = argent - final;
+
+//     }
+//         console.log("Argent :",argent," DH")
+//         console.log("Votre total est :",total," DH")
+//         console.log("Reduction :",red," DH")
+//         console.log("Total finale :",final," DH")
+//         console.log("Voici votre reste :",rest," DH")
+//         console.log("billet 200 DH :",rest /200)
+//         console.log("billet 100 DH :",rest % 200)
+        
+
+// }else{
+//     console.log("Argent insuffisant.")
+// }
+
+// let nbr = 10;
+// let choix = 1
+
+
+
+// while(choix != nbr){
+
+//     choix = parseInt(prompt("tapez un nombre :"))
+
+    
+//     if(choix > nbr){
+
+//         console.log("le nombre cache est inferieur a",choix)
+        
+
+//     }
+//     if(choix < nbr){
+
+//         console.log("nombre chaché est superieur a",choix)
+//     }
+        
+
+
+ 
+// }
+// console.log("Bravo le nombre est :",choix);
+
+// let note1 = 5;
+// let note2 = 10;
+// let note3 = 14;
+// let note4 = 17;
+// let note5 = 9;
+// let somme = note1 + note2 + note3 + note4 + note5
+// console.log("Somme :",somme)
+// console.log("moyenne :", somme / 5)
+
+do{
+    let fruits = [];
+    fruits.push(prompt("donnez un fruit :"))
+
+}while(fruits != 0){
+
+    console.log(fruits)
+    
+
+}
+console.log(fruits)
