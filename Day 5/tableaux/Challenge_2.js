8075043ea655902ea9be6283901f35162ebd522e
Omar <omarjador1@outlook.com>
2026-09-06 22:02:16 +0100

    Added Challenge 2 : Calculateur de Somme.

---
+++ Day 5/tableaux/Challenge_2.js
@@ -0,0 +1,12 @@
+function calculerSomme(tab){
+
+    somme = 0;
+    for(let i=0; i<tab.length; i++){
+     
+        somme += tab[i]
+
+    }return somme
+
+}
+
+console.log(calculerSomme([5,10,15]))
