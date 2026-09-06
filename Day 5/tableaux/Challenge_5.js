2d40cd13c076e7e885228bbb1b48c7818cf2530a
Omar <omarjador1@outlook.com>
2026-09-06 22:44:03 +0100

    Added Challenge 5 : Recherche d'Élément (Simulation de includes).

---
+++ Day 5/tableaux/Challenge_5.js
@@ -0,0 +1,15 @@
+function contientElement(tab, valeur){
+    let reponse =""
+    for(i=0; i<tab.length; i++){
+
+        if(tab[i] === valeur){
+            reponse = true
+        }else{
+            reponse = false
+        }
+
+    }
+    return reponse
+}
+
+console.log(contientElement(["pomme", "orange"], "banane"))
