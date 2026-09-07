e928f33f43f228084db99079c2b8a3b4f26fe3e2
Omar <omarjador1@outlook.com>
2026-09-07 15:31:11 +0100

    Added Challenge 9 : Fusion de Deux Tableaux (Simulation de concat).

---
+++ Day 5/tableaux/Challenge_9.js
@@ -0,0 +1,21 @@
+function fusionnerTableaux(tab1, tab2){
+    let tab3 = [];
+    let max = tab1.length + tab2.length
+    let counter = 0;
+    for(i=0; i<tab1.length; i++){
+
+        tab3[counter] = tab1[i]
+        counter++
+        
+    }
+    let tab3length = tab3.length
+    for(j=0; j<tab2.length; j++){
+
+        tab3[tab3length] = tab2[j]
+        tab3length++
+
+    }
+    
+    console.log(tab3)
+}
+fusionnerTableaux([1,2,3],[4,5,6])
