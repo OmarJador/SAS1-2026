9cd66f044b7ebb847f14c2b8515fbdf511b86d26
Omar <omarjador1@outlook.com>
2026-09-07 11:40:52 +0100

    Modified Challenge_1.js

--- Day 5/tableaux/Challenge_1.js
+++ Day 5/tableaux/Challenge_1.js
@@ -1,8 +1,6 @@
 function afficherElements(tab){
-
-    for(let i =0; i<tab.length ;i++){
+    for(let i = 0; i<tab.length ;i++){
         console.log(tab[i])
     }
-
 }
-console.log(afficherElements([1,2,3,4,5]))
+afficherElements([10,20,30])
