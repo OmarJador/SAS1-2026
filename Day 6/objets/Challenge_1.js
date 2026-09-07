8f41a7c3491483aa42dee816fdc04aa85030e9eb
Omar <omarjador1@outlook.com>
2026-09-07 18:30:06 +0100

    Added Challenge 01 — Profil étudiant.

---
+++ Day 6/objets/Challenge_1.js
@@ -0,0 +1,13 @@
+let student = {
+    firstName: "Yassine",
+    lastName: "Amrani",
+    age: 21,
+    city: "Youssoufia",
+    school: "Youcode"
+}
+console.log("Prénom :",student.firstName)
+console.log("Nom :",student.lastName)
+console.log("Age :",student.age)
+console.log("Ville :",student.city)
+console.log("Ecole :",student.school)
+console.log(student.firstName, student.lastName, student.age, student.city, student.school)
